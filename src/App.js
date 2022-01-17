121f59a81254f4d9f8407d2356bc5fd7ae867ab9
Jonathan <joncat86@gmail.com>
2022-01-17 17:34:05 +0100

    try

--- src/App.js
+++ src/App.js
@@ -7,25 +7,15 @@ import VictimsPage from "./components/VictimsPage";
 import Error404 from "./components/Error404";
 import Nav from "./components/Nav";
 import TitleBar from "./components/TitleBar";
-import { useEffect } from "react";
 import Footer from "./components/Footer";
 
 function App() {
-  //   useEffect(() => {
-  //     const fetchData = () => {
-  //       const data = fetch(texts);
-  //       const textsData = data.json;
-  //       return textsData;
-  //     };
-  //     fetchData();
-  //   }, []);
-
   return (
     <Router>
       <TitleBar />
       <Nav />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<HomePage />} />
         <Route path="/characters" exact element={<CharaPage />} />
         <Route path="/films" exact element={<FilmsPage />} />
         <Route path="/victims" exact element={<VictimsPage />} />
