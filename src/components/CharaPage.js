052576928fa79896620067ef8bb3bd16fdb592d4
Jonathan <joncat86@gmail.com>
2022-01-19 18:14:36 +0100

    film pics, characters top, bonus

--- src/components/CharaPage.js
+++ src/components/CharaPage.js
@@ -4,6 +4,7 @@ import texts from "../assets/texts/charasTexts.json";
 const CharaPage = () => {
   return (
     <section className="chara-page">
+      <div className="chara-top"></div>
       {texts.map((el) => (
         <Chara name={el.name} pic={el.pic} films={el.films} comment={el.text} />
       ))}
--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
 // import logo from "../images/logo.png";
 import logo from "../images/logounderconstruction.png";
+import riure from "../images/riure.png";
 
 const TitleBar = () => {
   return (
     <div className="title-bar">
-      <img src={logo} alt="logo" />
+      <Link to="/">
+        <img className="logo" src={logo} alt="logo" />
+      </Link>
+      <a
+        className="bonus-link"
+        href="https://www.youtube.com/watch?v=cQMzwiBJyY0"
+        target="_blank"
+        rel="noreferrer noopen"
+      >
+        <img className="riure" src={riure} alt="laugh" />
+      </a>
     </div>
   );
 };
